be683d8a69853cd6c26c81adfe5eaecc4a551f79
Anugerah Ashar <hutanalaska382@gmail.com>
2023-05-08 17:28:22 +0800

    Penambahan method dari class di package lain, perubahan file Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
 import koneksi.Database;
+import model.Dosen;
+import model.Mahasiswa;
+import model.MataKuliah;
 public class Main{
   public static void main(String[] args){
     System.out.println("Ini program utama");
     Database.getConnection();
+    Dosen.tampilanInfo();
+    Mahasiswa.tampilanInfo();
+    MataKuliah.tampilanInfo();
   }
 }
